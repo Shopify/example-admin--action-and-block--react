f2a64bb7d5c03afd843c0cb8dd49136ca8263103
Mitch Lillie <mitch.lillie@shopify.com>
2023-12-14 10:40:06 -0800

    Add step 1

--- extensions/issue-tracker-action/src/ActionExtension.jsx
+++ extensions/issue-tracker-action/src/ActionExtension.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import {
   reactExtension,
   useApi,
@@ -8,100 +8,24 @@ import {
   TextArea,
   Box,
 } from "@shopify/ui-extensions-react/admin";
-import { getIssues, updateIssues } from "./utils";
-
-// The target used here must match the target used in the extension's .toml file at ./shopify.ui.extension.toml
+// The target used here must match the target used in the extension's .toml file at ./shopify.extension.toml
 const TARGET = "admin.product-details.action.render";
 
 export default reactExtension(TARGET, () => <App />);
 
 function App() {
-  const { close, data, intents } = useApi(TARGET);
-  const issueId = intents?.launchUrl
-    ? new URL(intents?.launchUrl)?.searchParams?.get("issueId")
-    : null;
-  const [loading, setLoading] = useState(issueId ? true : false);
+  // Connect with the extension's APIs
+  const { close } = useApi(TARGET);
   const [issue, setIssue] = useState({ title: "", description: "" });
-  const [allIssues, setAllIssues] = useState([]);
-  const [formErrors, setFormErrors] = useState(null);
   const { title, description, id } = issue;
   const isEditing = id !== undefined;
 
-  // [START action-extension.fetch]
-  useEffect(() => {
-    (async function getProductInfo() {
-      const productData = await getIssues(data.selected[0].id);
-      setLoading(false);
-      if (productData?.data?.product?.metafield?.value) {
-        setAllIssues(JSON.parse(productData.data.product.metafield.value));
-      }
-    })();
-  }, []);
-  // [END action-extension.fetch]
-
-  const generateId = () => {
-    if (!allIssues.length) {
-      return 0;
-    }
-    return allIssues[allIssues.length - 1].id + 1;
-  };
-
-  const validateForm = () => {
-    setFormErrors({
-      title: !title,
-      description: !description,
-    });
-    return Boolean(title) && Boolean(description);
-  };
-
   const onSubmit = useCallback(async () => {
-    if (validateForm()) {
-      const newIssues = [...allIssues];
-      if (isEditing) {
-        // Find the index of the issue that you're editing
-        const editingIssueIndex = newIssues.findIndex(
-          (listIssue) => listIssue.id == issue.id
-        );
-        // Overwrite that issue's title and description with the new ones
-        newIssues[editingIssueIndex] = {
-          ...issue,
-          title,
-          description,
-        };
-      } else {
-        // Add a new issue at the end of the list
-        newIssues.push({
-          id: generateId(),
-          title,
-          description,
-          completed: false,
-        });
-      }
-
-      // Commit changes to the database
-      await updateIssues(data.selected[0].id, newIssues);
-      // Close the modal
-      close();
-    }
-  }, [issue, setIssue, allIssues, title, description]);
-
-  useEffect(() => {
-    if (issueId) {
-      // If opened from the block extension, you find the issue that's being edited
-      const editingIssue = allIssues.find(({ id }) => `${id}` === issueId);
-      if (editingIssue) {
-        // Set the issue's ID in the state
-        setIssue(editingIssue);
-      }
-    }
-  }, [issueId, allIssues]);
-
-  if (loading) {
-    return <></>;
-  }
+    console.log('submitting')
+  }, []);
 
   return (
-    // [START action-extension.ui]
+    // The UX is defined using components from the @shopify/ui-extensions-react/admin package
     <AdminAction
       title={isEditing ? "Edit your issue" : "Create an issue"}
       primaryAction={
@@ -111,16 +35,12 @@ function App() {
     >
       <TextField
         value={title}
-        error={formErrors?.title ? "Please enter a title" : undefined}
         onChange={(val) => setIssue((prev) => ({ ...prev, title: val }))}
         label="Title"
       />
       <Box paddingBlockStart="large">
         <TextArea
           value={description}
-          error={
-            formErrors?.description ? "Please enter a description" : undefined
-          }
           onChange={(val) =>
             setIssue((prev) => ({ ...prev, description: val }))
           }
@@ -128,6 +48,5 @@ function App() {
         />
       </Box>
     </AdminAction>
-    // [END action-extension.ui]
   );
 }
--- extensions/issue-tracker-block/src/BlockExtension.jsx
+++ extensions/issue-tracker-block/src/BlockExtension.jsx
@@ -3,18 +3,16 @@ import {
   Box,
   Button,
   Divider,
-  Form,
   Icon,
   InlineStack,
-  Select,
   Text,
   reactExtension,
   useApi,
 } from "@shopify/ui-extensions-react/admin";
 import { useEffect, useMemo, useState } from "react";
-import { getIssues, updateIssues } from "./utils";
+import { getIssues } from "./utils";
 
-// The target used here must match the target used in the extension's .toml file at ./shopify.ui.extension.toml
+// The target used here must match the target used in the extension's .toml file at ./shopify.extension.toml
 const TARGET = "admin.product-details.block.render";
 
 export default reactExtension(TARGET, () => <App />);
@@ -22,9 +20,8 @@ export default reactExtension(TARGET, () => <App />);
 const PAGE_SIZE = 5;
 
 function App() {
-  const { navigation, data } = useApi(TARGET);
+  const { data, i18n } = useApi(TARGET);
   const [loading, setLoading] = useState(true);
-  const [initialValues, setInitialValues] = useState([]);
   const [issues, setIssues] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
 
@@ -42,49 +39,10 @@ function App() {
         const parsedIssues = JSON.parse(
           productData.data.product.metafield.value
         );
-        setInitialValues(
-          parsedIssues.map(({ completed }) => Boolean(completed))
-        );
         setIssues(parsedIssues);
       }
     })();
   }, []);
-
-  const handleDelete = async (id) => {
-    // Create a new array of issues leaving out the one that's being deleted
-    const newIssues = issues.filter((issue) => issue.id !== id);
-    // Save to the local state
-    setIssues(newIssues);
-    // Commit changes to the database
-    await updateIssues(productId, newIssues);
-  };
-
-  const handleChange = async (id, value) => {
-    // Update the local state of the extension to reflect changes
-    setIssues((currentIssues) => {
-      // Create a copy of the array so you don't mistakenly mutate the state
-      const newIssues = [...currentIssues];
-      // Find the index of the issue that you're interested in
-      const editingIssueIndex = newIssues.findIndex(
-        (listIssue) => listIssue.id == id
-      );
-      // Overwrite that item with the new value
-      newIssues[editingIssueIndex] = {
-        // Spread the previous item to retain the values that you're not changing
-        ...newIssues[editingIssueIndex],
-        // Update the completed value
-        completed: value === "completed" ? true : false,
-      };
-      return newIssues;
-    });
-  };
-
-  const onSubmit = async () => {
-    // Commit changes to the database
-    await updateIssues(productId, issues);
-  };
-
-  const onReset = () => {};
 
   const paginatedIssues = useMemo(() => {
     if (issuesCount <= PAGE_SIZE) {
@@ -106,139 +64,76 @@ function App() {
   const summary = `${issuesCount} ${issuesCount === 1 ? "issue" : "issues"}`
 
   return (
-    // [START block-extension.ui]
     <AdminBlock
-      title="Issues"
+      // Translate the block title with the i18n API, which uses the strings in the locale files
+      title={i18n.translate("name")}
+      // It's best UX practice to set an empty summary when there's no data
       summary={summary}
     >
-    {/* [END block-extension.ui] */}
-    {/* [START block-extension.save-bar] */}
-      <Form id={`issues-form`} onSubmit={onSubmit} onReset={onReset}>
-        {issues.length ? (
-          <>
-            {paginatedIssues.map(
-              ({ id, title, description, completed }, index) => {
-                return (
-                  <>
-                    {index > 0 && <Divider />}
-                    <Box key={id} padding="base small">
-                      <InlineStack
-                        blockAlignment="center"
-                        inlineSize="100%"
-                        gap="large"
-                      >
-                        <Box inlineSize="53%">
-                          <Box inlineSize="100%">
-                            <Text fontWeight="bold">{title}</Text>
-                          </Box>
+      {issues.length ? (
+        <>
+          {paginatedIssues.map(
+            ({ id, title, description, completed }, index) => {
+              return (
+                <>
+                  {index > 0 && <Divider />}
+                  <Box key={id} padding="base small">
+                    <InlineStack
+                      blockAlignment="center"
+                      inlineSize="100%"
+                      gap="large"
+                    >
+                      <Box inlineSize="78%">
+                        <Box inlineSize="100%">
+                          <Text fontWeight="bold">{title}</Text>
+                        </Box>
 
-                          <Box inlineSize="100%">
-                            <Text>{truncate(description, 35)}</Text>
-                          </Box>
+                        <Box inlineSize="100%">
+                          <Text>{truncate(description, 35)}</Text>
                         </Box>
-                        <Box inlineSize="22%">
-                          <Select
-                            label="Status"
-                            name="status"
-                            defaultValue={
-                              initialValues[index] ? "completed" : "todo"
-                            }
-                            value={completed ? "completed" : "todo"}
-                            onChange={(value) => handleChange(id, value)}
-                            options={[
-                              { label: "Todo", value: "todo" },
-                              {
-                                label: "Completed",
-                                value: "completed",
-                              },
-                            ]}
-                          />
-                        </Box>
-                        <Box inlineSize="25%">
-                          <InlineStack
-                            inlineSize="100%"
-                            blockAlignment="center"
-                            inlineAlignment="end"
-                            gap="base"
-                          >
-                            <Button
-                              variant="tertiary"
-                              onPress={() =>
-                                navigation?.navigate(
-                                  `extension:issue-tracker-action?issueId=${id}`
-                                )
-                              }
-                            >
-                              <Icon name="EditMinor" />
-                            </Button>
-                            <Button
-                              onPress={() => handleDelete(id)}
-                              variant="tertiary"
-                            >
-                              <Icon name="DeleteMinor" />
-                            </Button>
-                          </InlineStack>
-                        </Box>
-                      </InlineStack>
-                    </Box>
-                  </>
-                );
-              }
-            )}
-            <Divider />
-            <Box paddingBlockStart="base">
-            {/* [START block-extension.navigate] */}
-              <Button
-                onPress={() =>
-                  navigation?.navigate(`extension:issue-tracker-action`)
-                }
-              >
-                Add issue
-              </Button>
-            {/* [END block-extension.navigate] */}
-            </Box>
+                      </Box>
+                      <Box inlineSize="22%">
+                        {completed ? "completed" : "todo"}
+                      </Box>
+                    </InlineStack>
+                  </Box>
+                </>
+              );
+            }
+          )}
+          <InlineStack
+            paddingBlockStart="large"
+            blockAlignment="center"
+            inlineAlignment="center"
+          >
+            <Button
+              onPress={() => setCurrentPage((prev) => prev - 1)}
+              disabled={currentPage === 1}
+            >
+              <Icon name="ChevronLeftMinor" />
+            </Button>
             <InlineStack
-              paddingBlockStart="large"
+              inlineSize={25}
               blockAlignment="center"
               inlineAlignment="center"
             >
-              <Button
-                onPress={() => setCurrentPage((prev) => prev - 1)}
-                disabled={currentPage === 1}
-              >
-                <Icon name="ChevronLeftMinor" />
-              </Button>
-              <InlineStack
-                inlineSize={25}
-                blockAlignment="center"
-                inlineAlignment="center"
-              >
-                <Text>{currentPage}</Text>
-              </InlineStack>
-              <Button
-                onPress={() => setCurrentPage((prev) => prev + 1)}
-                disabled={currentPage >= totalPages}
-              >
-                <Icon name="ChevronRightMinor" />
-              </Button>
+              <Text>{currentPage}</Text>
             </InlineStack>
-          </>
-        ) : (
-          <>
-            <Box paddingBlockEnd="large">
-              <Text fontWeight="bold">No issues for this product</Text>
-            </Box>
             <Button
-              onPress={() =>
-                navigation?.navigate(`extension:issue-tracker-action`)
-              }
+              onPress={() => setCurrentPage((prev) => prev + 1)}
+              disabled={currentPage >= totalPages}
             >
-              Add your first issue
+              <Icon name="ChevronRightMinor" />
             </Button>
-          </>
-        )}
-      </Form>
-      {/* [END block-extension.save-bar] */}
+          </InlineStack>
+        </>
+      ) : (
+        <>
+          <Box paddingBlockEnd="large">
+            <Text fontWeight="bold">No issues for this product</Text>
+          </Box>
+        </>
+      )}
     </AdminBlock>
   );
 }
