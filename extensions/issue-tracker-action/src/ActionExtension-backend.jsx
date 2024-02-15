65c1bb2a13ded879797590a0b55aff8a3a54cd32
Elana Kopelevich <elana.kopelevich@shopify.com>
2024-02-15 09:43:37 -0700

    Pass allIssues into generateId to fix id bug

--- extensions/issue-tracker-action/src/ActionExtension-backend.jsx
+++ extensions/issue-tracker-action/src/ActionExtension-backend.jsx
@@ -98,7 +98,7 @@ function App() {
       } else {
         // Add a new issue at the end of the list
         newIssues.push({
-          id: generateId(),
+          id: generateId(allIssues),
           title,
           description,
           completed: false,
