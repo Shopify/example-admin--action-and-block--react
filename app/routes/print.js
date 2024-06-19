a974e38638b9b81cac2f7ffe3191fbc401a4c2d3
Aaron Springut <springut@Aarons-MacBook-Pro.local>
2024-06-19 08:40:17 -0700

    fixed await/type error

--- app/routes/print.js
+++ app/routes/print.js
@@ -4,15 +4,11 @@ export async function loader({ request }) {
   // [START build-admin-print-action.print-src-four-a]
   const { cors, admin } = await authenticate.admin(request);
   // [END build-admin-print-action.print-src-four-a]
-  // [START build-admin-print-action.query-params]
   const url = new URL(request.url);
   const query = url.searchParams;
   const docs = query.get("printType").split(",");
   const orderId = query.get("orderId");
-  // [END build-admin-print-action.query-params]
-  // [START build-admin-print-action.graphQL-query]
-  const orderDetails = await admin
-    .graphql(
+  const response = await admin.graphql(
       `query getOrder($orderId: ID!) {
       order(id: $orderId) {
         name,
@@ -29,14 +25,12 @@ export async function loader({ request }) {
           orderId: orderId,
         },
       }
-    )
-    .json();
-
+    );
+  const orderDetails = await response.json();
   const order = orderDetails.data.order;
-  // [END build-admin-print-action.graphQL-query]
-  // [START build-admin-print-action.print-src-four-b]
   const pages = docs.map((x) => orderPage(x, order));
   const print = printHTML(pages);
+  // [START build-admin-print-action.print-src-four-b]
   return cors(
     new Response(print, {
       status: 200,
